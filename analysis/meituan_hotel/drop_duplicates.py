446ebe6a25348195f8812188c2c282759516289c
weiDong ZHang <doter1995@qq.com>
2019-07-25 00:43:22 +0800

    update duplicates

--- analysis/meituan_hotel/drop_duplicates.py
+++ analysis/meituan_hotel/drop_duplicates.py
@@ -1,5 +1,16 @@
+import os
+from datetime import datetime
+
 import pandas as pd
 
-csv_data = pd.read_csv('dataSet_item.csv')
-csv_data = csv_data.drop_duplicates("id")
-csv_data.to_csv('dataSet_item1.csv', index=0)
+
+def drop_Url_duplicates(path):
+    csv_data = pd.read_csv(path + '/data_url.csv')
+    csv_data["id"] = csv_data["url"].map(lambda x: x.split("/")[3])
+    csv_data = csv_data.drop_duplicates("id")
+    csv_data.to_csv(path + '/data_url.csv', index=0, columns=["id", "url"])
+
+
+path = os.path.abspath("../../dataSet/{date}/".format(
+    date=datetime.now().strftime('%Y-%m-%d')))
+drop_Url_duplicates(path)
